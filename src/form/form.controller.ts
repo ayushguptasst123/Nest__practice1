43789f4fc96b68635c01b84d607e3002412aedc5
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-03-28 14:31:51 +0530

    using without DI

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -1,13 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserDto } from './dto/create.user.dto';
+import { FormService } from './form.service';
 
 @Controller('form')
 export class FormController {
+  private readonly formService: FormService;
+  constructor() {
+    this.formService = new FormService();
+  }
+
   @Post('/create')
   createNewUser(@Body() user: UserDto) {
     console.log('You did something: ', user);
     console.log(typeof user.phoneNumber);
 
     return 'You did it';
+  }
+
+  @Get()
+  showAllUsers() {
+    console.log(this.formService.findAll());
   }
 }
--- src/form/form.service.ts
+++ src/form/form.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
 
-@Injectable()
-export class FormService {}
+// @Injectable()
+export class FormService {
+  private readonly users = [{ id: 1, name: 'John Doe' }];
+
+  findAll() {
+    return this.users;
+  }
+}
