207aeefe1de4c080dbc9cb9cac81fa308020e960
Ayush Gupta <ayushkumar9783@gmail.com>
2026-04-09 23:19:31 +0530

    create library card done

--- src/library-card/library-card.controller.ts
+++ src/library-card/library-card.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { LibraryCardService } from './library-card.service';
+import { CreateLibraryCardDto } from './dtos/create-library-card.dto';
+import { CurrentStudent } from 'src/auth/decorators/current-student.decorator';
+import { Student } from 'src/students/entities/student.entity';
 
 @Controller('library-card')
-export class LibraryCardController {}
+export class LibraryCardController {
+  constructor(private libraryCardService: LibraryCardService) {}
+
+  @Post('/create')
+  createNewLibraryCard(
+    @Body() createLibraryCardDto: CreateLibraryCardDto,
+    @CurrentStudent() currentStudent: Student,
+  ) {
+    return this.libraryCardService.createNewLibraryCard(
+      createLibraryCardDto,
+      currentStudent,
+    );
+  }
+}
--- src/library-card/library-card.service.ts
+++ src/library-card/library-card.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CreateLibraryCardDto } from './dtos/create-library-card.dto';
+import { Student } from 'src/students/entities/student.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LibraryCard } from './entity/library-card.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class LibraryCardService {}
+export class LibraryCardService {
+  constructor(
+    @InjectRepository(LibraryCard)
+    private libraryCardRepository: Repository<LibraryCard>,
+  ) {}
+
+  createNewLibraryCard(
+    createLibraryCardDto: CreateLibraryCardDto,
+    currentStudent: Student,
+  ) {
+    const card = this.libraryCardRepository.create(createLibraryCardDto);
+    card.student = currentStudent;
+    return this.libraryCardRepository.save(card);
+  }
+}
