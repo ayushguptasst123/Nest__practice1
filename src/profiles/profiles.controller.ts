d7ab377441af1c75333c70b5b14b5c060774d343
Ayush Gupta <ayushgupta.silversky@gmail.com>
2026-04-11 10:56:59 +0530

    create Profiel API DONE

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProfilesService } from './profiles.service';
+import { CreateProfileDto } from './dtos/create.profile.dto';
+import { Public } from 'src/decorator/public.decorator';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  constructor(private profilesService: ProfilesService) {}
+
+  @Post()
+  @Public()
+  createProfile(@Body() createProfileDto: CreateProfileDto) {
+    return this.profilesService.create(createProfileDto);
+  }
+}
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Profile } from './entity/profile.entity';
+import { CreateProfileDto } from './dtos/create.profile.dto';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+  constructor(
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+  ) {}
+
+  create(createProfileDto: CreateProfileDto) {
+    const profile = this.profileRepository.create(createProfileDto);
+    return this.profileRepository.save(profile);
+  }
+}
